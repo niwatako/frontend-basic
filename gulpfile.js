224e8dbcd2db174497d870b8af10a77ef8196467
sshono <s.shosho1210@gmail.com>
2017-03-17 21:30:31 +0900

    サンプル

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,9 @@
-//gulpコマンドをつくる練習
+//gulpの宣言
+var gulp = require("gulp");
 
-var gulp = require("gulp"); //gulp使うよ、宣言
-
-gulp.task("default", function () {
-    console.log("Hello World!"); //$gulp を実行すると"Hello World!"と出力される
-});
-
-//Scssのコンパイル用コマンドを作る
-var sass = require("gulp-sass");
-
-gulp.task("scss", function(){
-    gulp.src(["./frontend/assets/scss/*.scss"]) //srcで対象フォルダを指定して、（*.scssはフォルダ直下のファイルを指す）
-        .pipe(sass()) //Sassのコンパイル機能をとってきて、
-        .pipe(gulp.dest("public/assets/css")); //場所を指定して保存
-});
 
 //pugのコンパイル用コマンド
 var pug = require("gulp-pug");
-
 gulp.task("pug", function () {
     gulp.src(["./frontend/assets/tmpl/*.pug", "!./frontend/assets/tmpl/_*.pug"])
         .pipe(pug({
@@ -26,10 +12,21 @@ gulp.task("pug", function () {
         .pipe(gulp.dest("public"))
 });
 
-//２つのタスクをいっぺんにやる
+
+//Scssのコンパイル用コマンド
+var sass = require("gulp-sass");
+gulp.task("scss", function(){
+    gulp.src(["./frontend/assets/scss/*.scss"]) //srcで対象フォルダを指定して、（*.scssはフォルダ直下のファイルを指す）
+        .pipe(sass()) //Sassのコンパイル機能をとってきて、
+        .pipe(gulp.dest("public/assets/css")); //場所を指定して保存
+});
+
+
+//２つのタスクをいっぺんに
 gulp.task("build", ["scss", "pug"]);
 
-//変更をリアルタイムで監視する
+
+//変更を監視する
 gulp.task("watch", function(){
     //第一引数：監視するファイル、第二引数：実行するタスク
     gulp.watch(["./frontend/assets/scss/**/*.scss"], ["scss"]);
